54f904215129dedf7a974eaf9012ef80d634512e
Daniel Marques <marques.daniel@outlook.com.br>
2022-04-29 08:36:57 +0100

    Update Vendedor

--- GV/Program.cs
+++ GV/Program.cs
@@ -1,12 +1,7 @@
-﻿namespace GV
+﻿namespace Gestor_Vendas
 {
     class Program
     {
-
-        static void Main(string[] args)
-        {
-            MenuPrincipal();
-        }
 
         //Estrutura dos dados de Venda e Vendedores.
         //Vendedores
@@ -16,7 +11,7 @@
             public string nomVendedor;
             public decimal comissaoVendedor;
         }
-        
+
         //Venda
         struct Venda
         {
@@ -25,197 +20,328 @@
             public decimal valorVenda;
         }
 
-        //Função para validar um numero.
-        static int Validacao(int menor, int maior, bool msg)
-        {
-            int num;
-            string valor;
-            bool t, ok = false;
 
-            //Ciclo de validação.
-            do
+        static void Main(string[] args)
+        {
+            MenuPrincipal();
+
+
+
+
+            //Função para validar um numero.
+            static int Validacao(int menor, int maior, bool msg)
             {
-                if (msg)
-                    Console.Write($"Escrever um número entre {menor} e {maior}: ");
 
-                valor = Console.ReadLine();
+                int num;
+                string valor;
+                bool t, ok = false;
 
-                //Testa o conteúdo da variável.
-                t = int.TryParse(valor, out num);
+                //Ciclo de validação.
+                do
+                {
+                    if (msg)
+                        Console.Write($"Escrever um número entre {menor} e {maior}: ");
 
-                //Validação do intervalo númerico.
-                if (!t || num < menor || num > maior)
-                    Console.WriteLine("Valor inválido, insira novamente!");
-                else
-                    ok = true;
+                    valor = Console.ReadLine();
 
-            } while (!ok);
+                    //Testa o conteúdo da variável.
+                    t = int.TryParse(valor, out num);
 
-            //Retorno do número validado.
-            return num;
-        }
+                    //Validação do intervalo númerico.
+                    if (!t || num < menor || num > maior)
+                        Console.WriteLine("Valor inválido, insira novamente!");
+                    else
+                        ok = true;
 
-        // Função que valida se codigo do Vendedor existe.
-        static bool ValidaCodExisteVendedor(int codigo, Vendedor[] vendedor)
-        {
-            
-            //Se encontrar passa a true.
-            bool encontrei = false; 
+                } while (!ok);
 
-            int i = 0;
-          
-            //Clico para encontrar se o número do Vendedor existe.
-            while (i < vendedor.Length && !encontrei)
+                //Retorno do número validado.
+                return num;
+            }
+
+            // Função que valida se codigo do Vendedor existe.
+            static bool ValidaCodExisteVendedor(int codigo, Vendedor[] vendedor)
             {
-                if (vendedor[i].codVendedor == codigo)
-                    encontrei = true;
-                i++; 
+
+                //Se encontrar passa a true.
+                bool encontrei = false;
+
+                int i = 0;
+
+                //Clico para encontrar se o número do Vendedor existe.
+                while (i < vendedor.Length && !encontrei)
+                {
+                    if (vendedor[i].codVendedor == codigo)
+                        encontrei = true;
+                    i++;
+                }
+                return encontrei;
             }
-            return encontrei;
-        }
 
-        //Valida se o número existe e
-        //toma a decisão de aceitar ou não de acordo com o tipo.
-        static int ValidaCodigoVendedor(int limEsq, int limDir, Vendedor[] vendedor, string tipo)
-        {
-            
-            int codigo = 0;
+            //Valida se o número existe e
+            //toma a decisão de aceitar ou não de acordo com o tipo.
+            static int ValidaCodigoVendedor(int limEsq, int limDir, Vendedor[] vendedor, string tipo)
+            {
 
-            while (true)
-            { 
-                codigo = Validacao(limEsq, limDir, true);
+                int codigo = 0;
 
-                //ValidaCodExisteVendedor o código no array.
-                //True se encontra ou F se não encontra.
-                bool result = ValidaCodExisteVendedor(codigo, vendedor);
+                while (true)
+                {
+                    codigo = Validacao(limEsq, limDir, true);
 
-       
-                if (result == true && tipo == "new")
-                {
-                    Console.WriteLine($"Código = {codigo} existe!");
+                    //ValidaCodExisteVendedor o código no array.
+                    //True se encontra ou F se não encontra.
+                    bool result = ValidaCodExisteVendedor(codigo, vendedor);
+
+
+                    if (result == true && tipo == "new")
+                    {
+                        Console.WriteLine($"Código {codigo} existe!");
+                    }
+
+                    else if (result == false && tipo == "new")
+                    {
+                        Console.WriteLine($"Código {codigo} não existe!");
+                        break;
+                    }
+
+                    if (result == true && tipo == "exist")
+                    {
+                        Console.WriteLine($"Código {codigo} existe!");
+                        break;
+                    }
+
+                    else if (result == false && tipo == "exist")
+                    {
+                        Console.WriteLine($"Código {codigo} não existe!");
+                    }
                 }
-                
-                else if (result == false && tipo == "new")
-                {
-                    Console.WriteLine($"Código = {codigo} não existe!");
-                    break;
-                }
-                
-                if (result == true && tipo == "exist")
-                {
-                    Console.WriteLine($"Código = {codigo} existe!");
-                    break;
-                }
-               
-                else if (result == false && tipo == "exist")
-                {
-                    Console.WriteLine($"Código = {codigo} não existe!");
-                }
+                return codigo;
             }
-            return codigo;
-        }
 
-        //Menu Principal
-        static void MenuPrincipal()
-        {
-            byte opMenu = 1;
+            //Menu Principal
 
-            while (opMenu != 0)
+            static void MenuPrincipal()
             {
                 Console.Clear();
-                Console.WriteLine("GV - GESTÃO DE VENDAS");
+                Console.WriteLine("GESTÃO DE VENDAS");
                 Console.WriteLine("\n1. Vendedor");
                 Console.WriteLine("2. Venda");
                 Console.WriteLine("\n0. Sair");
-                Console.Write("\nOpção: ");
-                opMenu = Convert.ToByte(Console.Read());
+                int op = Validacao(0, 2, true);
 
-
-                if (opMenu == 0)
-                    break;
-
-                //Menu Vendedor
-                else if (opMenu == 1)
+                switch (op)
                 {
-                    Console.Clear();
-                    Console.WriteLine("MENU VENDEDOR");
-                    Console.WriteLine("\n1. Inserir");
-                    Console.WriteLine("2. Consultar");
-                    Console.WriteLine("3. Listar");
-                    Console.WriteLine("4. Alterar");
-                    Console.WriteLine("5. Eliminar");
-                    Console.WriteLine("6. Comissão de Vendas");
-                    Console.WriteLine("0. Voltar");
+                    case 1:
+                        MenuVendedor();
+                        break;
+                    case 2:
+                        MenuVenda();
+                        break;
+                    case 0:
+                        Console.Clear();
+                        Console.WriteLine("Obrigado por utilizar a nossa aplicação.");
+                        Console.WriteLine("Tchau!");
+                        Environment.Exit(0);
+                        break;
+                }
+            }
 
-                    Console.Write("Opção: ");
-                    byte op = Convert.ToByte(Console.Read());
+            static void MenuVendedor()
+            {
+                Console.Clear();
+                Console.WriteLine("MENU VENDEDOR");
+                Console.WriteLine("\n1. Inserir");
+                Console.WriteLine("2. Consultar");
+                Console.WriteLine("3. Listar");
+                Console.WriteLine("4. Alterar");
+                Console.WriteLine("5. Eliminar");
+                Console.WriteLine("6. Comissão de Vendas");
+                Console.WriteLine("\n0. Voltar");
+                int opMenu = Validacao(0, 6, true);
 
 
-                    //Definição da estrutura para o Array Vendedor.
-                    Vendedor[] venda = new Vendedor[0];
+                //Definição da estrutura para o Array Vendedor.
+                Vendedor[] vendedor = new Vendedor[0];
+
+                //Variáveis Menu
+                int codigoVendedor = 0;
+
+                switch (opMenu)
+                {
+                    case 1: //Inserindo Vendedores.
+                        Console.Clear();
+                        Console.WriteLine("INSERIR VENDEDOR");
+                        Console.WriteLine("\nQuantos vendedores quer adicionar?");
+                        int numVendedores = Validacao(1, 1000, true);
+
+                        //Valida o tamanho antigo do Array Vendedor.
+                        int tamanhoAntigo = vendedor.Length;
+
+                        // Obtem o número total de índices do Array Vendedor.
+                        int tamanhoAtual = tamanhoAntigo + numVendedores;
+
+                        // Redimensiona o comprimento do Array Vendedor para a nova dimensão.
+                        Array.Resize(ref vendedor, tamanhoAtual);
+
+                        for (int i = tamanhoAntigo; i < tamanhoAtual; i++)
+                        {
+                            Console.WriteLine($"\nInserindo o {i + 1}º Vendedor");
+
+                            //Inserindo novos vendedores no Array Vendedor.
+                            Console.Write("Código: ");
+                            vendedor[i].codVendedor = ValidaCodigoVendedor(1, 1000, vendedor, "new");
+
+                            Console.Write("Nome: ");
+                            vendedor[i].nomVendedor = Console.ReadLine();
+
+                            Console.Write("Comissão: ");
+                            vendedor[i].comissaoVendedor = Convert.ToDecimal(Console.ReadLine());
+
+                        }
+
+                        Console.WriteLine("\nVendedores inseridos com sucesso!");
+                        Console.ReadKey();
+                        MenuVendedor();
+                        break;
 
 
-                    switch (op)
-                    {
-                        //Inserindo Vendedores.
-                        case 1:
-                            Console.Write("INSERIR VENDEDOR");
-                            Console.Write("Quantos vendedores quer adicionar? ");
-                            int numVendedores = Validacao(1, 1000, true);
+                    case 2: //Consultar Vendedor
+                        Console.Clear();
+                        Console.WriteLine("CONSULTAR VENDEDOR");
+                        Console.WriteLine("\nCódigo do Vendedor:");
+                        codigoVendedor = ValidaCodigoVendedor(1, 1000, vendedor, "exist");
 
-                            //Valida o tamanho antigo do Array Vendedor.
-                            int tamanhoAntigo = venda.Length;
-
-                            // Obtem o número total de índices do Array Vendedor.
-                            int tamanhoAtual = tamanhoAntigo + numVendedores;
-
-                            // Redimensiona o comprimento do Array Vendedor para a nova dimensão.
-                            Array.Resize(ref venda, tamanhoAtual);
-
-                            for (int i = tamanhoAntigo; i < tamanhoAtual; i++)
+                        for (int i = 0; i < vendedor.Length; i++)
+                            if (codigoVendedor == vendedor[i].codVendedor)
                             {
-                                Console.WriteLine($"Inserindo o(a) {i + 1}º Vendedor(a)");
 
-                                //Inserindo novos vendedores no Array Vendedor.
+                                Console.WriteLine($"Código: {vendedor[i].codVendedor}, " +
+                                    $"Nome: {vendedor[i].nomVendedor}, " +
+                                    $"Comissão: {vendedor[i].comissaoVendedor}");
+                            }
+                        
+                        
+                        MenuVendedor();
+                        break;
+
+                    case 3: // Listar Vendedores
+                        Console.Clear();
+                        Console.WriteLine("LISTAR VENDEDOR");
+                        for (int i = 1; i <= vendedor.Length; i++)
+                        {
+
+                            Console.WriteLine($"Código: {vendedor[i - 1].codVendedor}, " +
+                                $"Nome: {vendedor[i - 1].nomVendedor}, " +
+                                $"Comissão: {vendedor[i - 1].comissaoVendedor}");
+
+                            Console.WriteLine($"Codigo");
+
+                        }
+
+                        Console.ReadKey();
+                        MenuVendedor();
+                        break;
+
+                    case 4: //Alterar Vendedores
+                        Console.Clear();
+                        Console.WriteLine("ALTERAR VENDEDOR");
+                        Console.WriteLine("\nCódigo do Vendedor\n");
+                        codigoVendedor = ValidaCodigoVendedor(1, 1000, vendedor, "exist");
+
+                        for (int i = 0; i < vendedor.Length; i++)
+                            if (codigoVendedor == vendedor[i].codVendedor)
+                            {
+                                Console.WriteLine($"Código: {vendedor[i].codVendedor}, " +
+                                $"Nome: {vendedor[i].nomVendedor}, " +
+                                $"Comissão: {vendedor[i].comissaoVendedor}");
+
+                                Console.WriteLine("ALTERAÇÃO");
+
                                 Console.Write("Código: ");
-                                venda[i].codVendedor = ValidaCodigoVendedor(1, 1000, venda, "new");
+                                vendedor[i].codVendedor = ValidaCodigoVendedor(1, 1000, vendedor, "new");
 
                                 Console.Write("Nome: ");
-                                venda[i].nomVendedor = Console.ReadLine();
+                                vendedor[i].nomVendedor = Console.ReadLine();
 
                                 Console.Write("Comissão: ");
-                                venda[i].comissaoVendedor = Console.ReadLine();
+                                vendedor[i].comissaoVendedor = Convert.ToDecimal(Console.ReadLine());
                             }
-                            break;
-                    }
+
+                        Console.WriteLine("\nVendedores alterados com sucesso!");
+                        Console.ReadKey();
+                        MenuVendedor();
+                        break;
+
+                    case 5: //Eliminando colaborador.
+                        Console.Clear();
+                        Console.WriteLine("DELETAR VENDEDOR");
+                        Console.WriteLine("\nCódigo do Vendedor\n");
+                        codigoVendedor = ValidaCodigoVendedor(1, 1000, vendedor, "exist");
+
+                        for (int i = 0; i < vendedor.Length; i++)
+                        {
+                            if (codigoVendedor == vendedor[i].codVendedor)
+                            {
+
+                                Console.WriteLine($"Código: {vendedor[i].codVendedor}, " +
+                                    $"Nome: {vendedor[i].nomVendedor}, " +
+                                    $"Comissão: {vendedor[i].comissaoVendedor}");
+                            }
+
+                            Console.WriteLine("Eliminar vendedor? (S/n)");
+                            string eliminar = Console.ReadLine();
+
+                            if (eliminar == "S")
+                            {
+                                Console.WriteLine("Eliminando os dados...");
+                                for (int k = i; k < vendedor.Length - 1; k++)
+                                {
+                                    vendedor[k].codVendedor = vendedor[k + 1].codVendedor;
+                                    vendedor[k].nomVendedor = vendedor[k + 1].nomVendedor;
+                                    vendedor[k].comissaoVendedor = vendedor[k + 1].comissaoVendedor;
+                                }
+                            }
+                        }
+                        Console.WriteLine("Dados eliminados com sucesso!");
+                        Console.ReadKey();
+                        MenuVendedor();
+                        break;
+
+                    case 0:
+                        MenuPrincipal();
+                        break;
                 }
-
-                //Menu Venda               
-                else if (opMenu == 2)
-                {
-                    Console.Clear();
-                    Console.WriteLine("MENU VENDA");
-                    Console.WriteLine("\n1. Inserir");
-                    Console.WriteLine("2. Consultar");
-                    Console.WriteLine("3. Listar");
-                    Console.WriteLine("4. Alterar");
-                    Console.WriteLine("5. Eliminar");
-                    Console.WriteLine("6. Total de vendas por Vendedor");
-                    Console.WriteLine("7. Total de vendas por Zona");
-                    Console.WriteLine("8. Total de vendas por Mês");
-                    Console.WriteLine("9. Total de vendas por Mês para cada Vendedor");
-                    Console.WriteLine("0. Voltar");
-
-                    Console.ReadKey();
-
-
-                }
-                //Sair
-                else if (opMenu == 0)
-                    break;
             }
-        } 
 
+            static void MenuVenda()
+            {
+                Console.Clear();
+                Console.WriteLine("MENU VENDA");
+                Console.WriteLine("\n1. Inserir");
+                Console.WriteLine("2. Consultar");
+                Console.WriteLine("3. Listar");
+                Console.WriteLine("4. Alterar");
+                Console.WriteLine("5. Eliminar");
+                Console.WriteLine("6. Total de vendas por Vendedor");
+                Console.WriteLine("7. Total de vendas por Zona");
+                Console.WriteLine("8. Total de vendas por Mês");
+                Console.WriteLine("9. Total de vendas por Mês para cada Vendedor");
+                Console.WriteLine("\n0. Voltar");
+                int opMenu = Validacao(0, 9, true);
+
+
+                //Definição da estrutura para o Array Vendedor.
+                Venda[] venda = new Venda[0];
+
+                //Variáveis Menu
+                int codigoVenda;
+
+
+            }
+
+
+        }
     }
 }
     
